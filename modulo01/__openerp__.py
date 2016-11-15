4b569e244a99b0ac603d4e407b0ba87c93926ab3
Alvaro Villavicencio <alvaro.odoo@gmail.com>
2016-11-15 23:51:07 +0000

    [REF] openacademy: Adding Herencia

--- modulo01/__openerp__.py
+++ modulo01/__openerp__.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Open Academy",
+    'name': "Módulo de Ejemplo",
 
     'summary': """Manage trainings""",
 
     'description': """
-        Open Academy module for managing trainings:
+        Módulo01 for managing trainings:
             - training courses
             - training sessions
             - attendees registration
     """,
 
-    'author': "Your Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Alvaro Villavicencio Ramírez",
+    'website': "http://www.soluciones4gcom",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
@@ -25,17 +25,11 @@
 
     # always loaded
     'data': [
-        'view/openacademy_course_view.xml',
-        'view/openacademy_session_view.xml',
-        'view/partner_view.xml',
         # 'security/ir.model.access.csv',
         # 'templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/openacademy_course_demo.xml',
+        # 'demo.xml',
     ],
-    'installable': True,
-    'auto_install':True,
 }
-
