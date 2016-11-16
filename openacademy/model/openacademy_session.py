ed0ea00c2976f2d38bbbccf73a8f47ed415e369c
Alvaro Villavicencio <alvaro.odoo@gmail.com>
2016-11-16 17:58:11 +0000

    [REF] openacademy: Add Domains

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -9,7 +9,9 @@ class Session (models.Model):
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     instructor_id = fields.Many2one('res.partner', 
-                                     string="Instructor")
+                                     string="Instructor",
+                                     domain=['|',('instructor', '=', True),
+                                                 ('category_id.name', 'ilike', "Teacher")])
     course_id = fields.Many2one('openacademy.course',
                                  ondelete='cascade', 
                                  string="Course", 
